5d3d4d83f885f76f1b24e83fe0c145989d3edb52
Strider <fernando.herrera85@gmail.com>
2017-06-27 13:28:34 -0600

    Primer commit del proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+
+  nombre:string = "Fernando";
+  apellido:string = "Herrera";
+  
+}
